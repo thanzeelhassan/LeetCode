a3692f96ed1260e8411ddcdf7de4f753c2522b51
Thanzeel Hassan <46446324+thanzeelhassan@users.noreply.github.com>
2022-06-25 01:14:50 +0530

    Time: 149 ms (32.07%), Space: 13.9 MB (57.42%) - LeetHub

--- 326-power-of-three/326-power-of-three.py
+++ 326-power-of-three/326-power-of-three.py
@@ -5,6 +5,7 @@ class Solution:
         if n == 1:
             return 1
         x = math.log10(n)/ math.log10(3)
+        print(x)
         ans = pow(3,int(x))
         if ans == n:
             return 1
