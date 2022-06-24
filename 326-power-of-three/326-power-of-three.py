bd7502a27471ff49a5e2d851190fa6615d0f5114
Thanzeel Hassan <46446324+thanzeelhassan@users.noreply.github.com>
2022-06-25 00:52:20 +0530

    Time: 174 ms (20.73%), Space: 13.7 MB (96.19%) - LeetHub

--- 326-power-of-three/326-power-of-three.py
+++ 326-power-of-three/326-power-of-three.py
@@ -4,5 +4,10 @@ class Solution:
             return 0
         if n == 1:
             return 1
-        return self.isPowerOfThree(n/3)
-        
+        if n < 0:
+            return 0
+        x = math.log10(n)/ math.log10(3)
+        ans = pow(3,int(x))
+        if ans == n:
+            return 1
+        return 0
