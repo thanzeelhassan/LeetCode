3c46631d0e403361a1c2687a044672ed1c9f2856
Thanzeel Hassan <46446324+thanzeelhassan@users.noreply.github.com>
2022-07-25 13:56:59 +0530

    Time: 80 ms (67.44%), Space: 15.1 MB (15.92%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -0,0 +1,22 @@
+class Solution:
+    def helper(self,n,cost,dict1):
+        if n in dict1:
+            return dict1[n]
+        if n < 0:
+            dict1[n] = 0
+            return 0
+        if n == 0:
+            dict1[n] = cost[n]
+            return cost[n]
+        if n == 1:
+            dict1[n] = cost[n]
+            return cost[n]
+        
+        temp = cost[n] + min(self.helper(n-1,cost,dict1), self.helper(n-2,cost,dict1))
+        dict1[n] = temp
+        return temp
+        
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dict1 = {}
+        return min(self.helper(n-1,cost,dict1),self.helper(n-2,cost,dict1))
