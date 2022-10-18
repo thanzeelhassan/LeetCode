f7c171d7ebc981339c12297bb4e6e98937931338
Thanzeel Hassan <46446324+thanzeelhassan@users.noreply.github.com>
2022-10-18 13:24:54 +0530

    Time: 133 ms (47.94%), Space: 13.9 MB (73.26%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,18 @@
+def splitNum(num):           
+    num = [int(d) for d in str(num)]
+    pow = len(num)-1
+    res = []
+    for n in num:
+        res.append(n*10**pow)
+        pow -= 1                
+    return res
+        
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:        
+        n1 = splitNum(num1)
+        n2 = splitNum(num2)
+        ans = 0
+        for x in n1:
+            for y in n2:
+                ans = ans + x * y
+        return str(ans)
