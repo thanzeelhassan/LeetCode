b25d1ab88ee4f6c886443895812d474fbdc36f00
Thanzeel Hassan <thanzeelhassan@gmail.com>
2025-01-03 23:39:16 +0530

    1 Jan 2025

--- All Problems/1422. Maximum Score After Splitting a String.py
+++ All Problems/1422. Maximum Score After Splitting a String.py
@@ -4,13 +4,17 @@ class Solution:
         zero_sum = 0
         one_sum = 0
         n = len(s)
+        
         if s[0] == '0':
             zero_sum += 1
+        
         for i in range(1, n):
             if s[i] == '1':
                 one_sum += 1
+        
         ans = one_sum + zero_sum
         new_ans = ans
+
         for i in range(1, n-1):
             if s[i] == '1':
                 new_ans = new_ans - 1
@@ -21,4 +25,4 @@ class Solution:
                 ans = new_ans
         
         return ans
-        
+  
