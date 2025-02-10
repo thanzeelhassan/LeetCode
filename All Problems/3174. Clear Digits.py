57763493d7508ca0cbd63394286f52aebd1ef0ea
Thanzeel Hassan <thanzeelhassan@gmail.com>
2025-02-10 12:20:49 +0530

    10 Feb 2025 better code

--- All Problems/3174. Clear Digits.py
+++ All Problems/3174. Clear Digits.py
@@ -1,21 +1,11 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
-        s = s[::-1]
-        n = len(s)
-        count_digits = 0
-        count_alpha = 0
+        ans = []
 
-        ans = "" 
-        for i in range(n):
-            if s[i].isdigit() == True:
-                ans = ans + s[i]
-                count_digits = count_digits + 1
-            elif s[i].isalpha() == True:
-                if count_digits > 0:
-                    ans = ans[:-1]
-                    count_digits = count_digits - 1
-                else:
-                    count_alpha = count_alpha + 1
-                    ans = ans + s[i]
-        return ans[::-1]
-        
+        for char in s :
+            if char.isdigit() and len(ans) != 0:
+                ans.pop()
+            else:
+                ans.append(char)
+
+        return "".join(ans)
