3bb1b32d7d0b42f93321109266b914a7301f598c
Thanzeel Hassan <46446324+thanzeelhassan@users.noreply.github.com>
2025-11-04 14:32:24 +0530

    3610-FIND-X-SUM-OF-ALL-K-LONG-SUBARRAYS-I Stats: Time: 27 ms (50.39%), Space: 17.9 MB (59.74%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.py
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.py
@@ -1,52 +1,14 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> int:
+    def findXSum(self, nums, k, x):
         n = len(nums)
-        my_dict = {}
-        ans = []
-        for i in range(n):
-            if nums[i] in my_dict:
-                my_dict[nums[i]] = my_dict[nums[i]] + 1
-            else:
-                my_dict[nums[i]] = 1
+        result = []
 
-        if len(my_dict) < k :
-            return sum(nums)
-        temp = 0
-        count = 0
-        highest = 0
-        my_dict_copy = my_dict.copy()
-        while(count < k):
-            for key, value in my_dict.items():
-                if value > highest:
-                    highest = value
-                    temp = key
-                elif value == highest and key > temp:
-                    highest = value
-                    temp = key
-            ans.append(temp)
-            count = count + 1
-            highest = 0
-            del my_dict[temp]
+        for i in range(n - k + 1):
+            window = nums[i:i + k]
+            freq = Counter(window)
+            sorted_freq = sorted(freq.items(), key=lambda a: (-a[1], -a[0]))
+            top_x = set(num for num, _ in sorted_freq[:x])
+            x_sum = sum(num for num in window if num in top_x)
+            result.append(x_sum)
 
-        ans_sum = 0
-        for i in ans:
-            ans_sum = ans_sum + i * my_dict_copy[i]
-        return ans_sum 
-
-    def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
-        n = len(nums)
-        ans = [0] * (n - k + 1)
-        
-        my_dict = {}
-
-        for i in range(n):
-            if nums[i] in my_dict:
-                my_dict[nums[i]] = my_dict[nums[i]] + 1
-            else:
-                my_dict[nums[i]] = 1
-
-        for i in range(0, n - k + 1):
-            temp = self.topKFrequent(nums[i:i+k], x)
-            ans[i] = temp
-        
-        return ans
+        return result
