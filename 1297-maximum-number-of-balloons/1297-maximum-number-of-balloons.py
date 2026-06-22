cf77a9ad364520b96b1bdac8e2ae090a68931b7e
Thanzeel Hassan <46446324+thanzeelhassan@users.noreply.github.com>
2026-06-22 15:54:47 +0530

    1297-MAXIMUM-NUMBER-OF-BALLOONS Stats: Time: 3 ms (61.87%), Space: 19.2 MB (75.19%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
@@ -10,6 +10,6 @@ class Solution:
                 my_dict[text[i]] = 1
         
         if 'b' in my_dict and 'a' in my_dict and 'n' in my_dict and 'l' in my_dict and 'o' in my_dict:
-            return min(my_dict['b'], my_dict['a'], my_dict['n'], ceil(my_dict['l']/ 2), ceil(my_dict['o'] / 2));
+            return min(my_dict['b'], my_dict['a'], my_dict['n'], my_dict['l'] // 2, my_dict['o'] // 2);
         else: 
             return 0;
