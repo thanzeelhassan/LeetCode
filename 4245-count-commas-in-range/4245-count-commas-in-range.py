3b254bc64ac9ea2e3625155c6bc4b1f5fa57c017
Thanzeel Hassan <46446324+thanzeelhassan@users.noreply.github.com>
2026-09-09 05:20:50 -0700

    4245-COUNT-COMMAS-IN-RANGE Stats: Time: 0 ms (100%), Space: 19.1 MB (96.96%) - LeetHub

--- 4245-count-commas-in-range/4245-count-commas-in-range.py
+++ 4245-count-commas-in-range/4245-count-commas-in-range.py
@@ -2,7 +2,4 @@ class Solution:
     def countCommas(self, n: int) -> int:
         if n < 1000:
             return 0
-        if n < 10000:
-            return n - 1000 + 1
-        if n <= 100000:
-            return 9000 + n - 10000 + 1
+        return n - 999
